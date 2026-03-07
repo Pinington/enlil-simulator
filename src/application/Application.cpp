7c6eadf4cc47b8941e8713e68fff55d208835769
Pinington <georges5757c@gmail.com>
2026-03-07 18:56:20 +0100

    Started to add comments

--- src/application/Application.cpp
+++ src/application/Application.cpp
@@ -3,15 +3,19 @@
 
 int Application::run(int argc, char *argv[])
 {
+    // Create QT app
     QApplication app(argc, argv);
 
+    // Start a window
     QMainWindow window;
     window.resize(800, 600);
     window.setWindowTitle("Enlil Simulator");
 
+    // Start physics renderer and add to window
     Renderer* renderer = new Renderer();
     window.setCentralWidget(renderer);
     
+    // Show window
     window.show();
 
     return app.exec();
