4f2b8b16b6d28243551d86a17cf6111ed33b968a
Pinington <georges5757c@gmail.com>
2026-07-16 20:33:07 +0200

    Fixed forces initialization

--- src/physics/Engine.cpp
+++ src/physics/Engine.cpp
@@ -1,7 +1,10 @@
 #include "physics/Engine.h"
 
 void Engine::applyGravity() {
-    Force f {0.f, -9.f, 0.f};
+    Force f {
+        {0.f, -1.f, 0.f}, // direction
+        1                 // magnitude
+    };
     for (auto& p : particles.getSystem()) {
         p->applyForce(f);
     }
